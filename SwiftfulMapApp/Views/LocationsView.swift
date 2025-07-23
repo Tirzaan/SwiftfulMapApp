b8dd4c36f6c7c8ba676abfeffc0dfeed14c60948
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-23 11:03:26 -0500

    [Feature] Created Locations View

---
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  SwiftfulMapApp
+//
+//  Created by Tirzaan on 7/23/25.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
