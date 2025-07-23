bf343fa5ccf180f1a4a578729c20e5e549ffd0b1
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-23 10:26:47 -0500

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Tirzaan on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
