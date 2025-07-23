699f26c724902690f3934fe721d7c333b386aa90
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-23 11:24:47 -0500

    [Feature] Added viewModel to LocationsView

--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 
 struct LocationsView: View {
+    
+    @EnvironmentObject private var viewModel: LocationsViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(viewModel.locations) { location in
+                Text(location.name)
+            }
+        }
     }
 }
 
 #Preview {
     LocationsView()
+        .environmentObject(LocationsViewModel())
 }
