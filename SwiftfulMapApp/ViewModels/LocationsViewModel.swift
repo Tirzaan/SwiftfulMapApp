97e72eeeb7f5de4b709e59402ff78978188a72cf
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-23 11:56:46 -0500

    [Feature] Added Map Location Managing

--- SwiftfulMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftfulMapApp/ViewModels/LocationsViewModel.swift
@@ -6,13 +6,44 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
     
     @Published var locations: [Location]
+    @Published var currentLocation: Location {
+        didSet {
+            updateMapRegion(location: currentLocation)
+        }
+    }
+    @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+    
+    let mapSpan: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
     
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        if let firstLocation = locations.first {
+            self.currentLocation = firstLocation
+        } else {
+            self.currentLocation = Location(
+                name: "No Location Found",
+                cityName: "None",
+                coordinates: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+                description: "No description",
+                imageNames: [],
+                link: "")
+        }
+        self.updateMapRegion(location: currentLocation)
+    }
+    
+    private func updateMapRegion (location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(
+                center: location.coordinates,
+                span: mapSpan
+            )
+        }
     }
 }
