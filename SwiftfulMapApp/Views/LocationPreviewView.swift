7fa1743623d500272bf1343562e36563d8d95e55
Tirzaan Neil <Tirzaan@tntneil.com>
2025-07-23 14:54:17 -0500

    [Feature] Created LocationPreviewView

---
+++ SwiftfulMapApp/Views/LocationPreviewView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationPreviewView.swift
+//  SwiftfulMapApp
+//
+//  Created by Tirzaan on 7/23/25.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationPreviewView()
+}
